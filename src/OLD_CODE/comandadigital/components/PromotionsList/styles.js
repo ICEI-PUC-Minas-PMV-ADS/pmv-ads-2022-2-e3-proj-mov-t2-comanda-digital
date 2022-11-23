af61736944e6a70dcc8fbd504446e594b1ce3100
André Vieira <91098261+avgropuc@users.noreply.github.com>
2022-11-23 20:20:28 -0300

    Delete src/OLD_CODE directory

--- src/OLD_CODE/comandadigital/components/PromotionsList/styles.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native'
-
-export default StyleSheet.create({
-  promotionsList: {
-    minHeight: '90%',
-    maxHeight: '90%',
-    backgroundColor: '#ffffff'
-  }
-})
